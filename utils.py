646ff60cc761a43d7496542ba3c64bf110551bfc
Yauhen Shulitski <jsnjack@gmail.com>
2015-02-14 15:10:35 +0300

    Add load config functions

---
+++ utils.py
@@ -0,0 +1,29 @@
+import os
+import pickle
+
+
+CONFIG_PATH = "./config/config.sb"
+
+
+def load_config():
+    """
+    Reads config from file
+    """
+    try:
+        with open(os.path.abspath(CONFIG_PATH), 'rb') as config_file:
+            config = pickle.load(config_file)
+    except IOError:
+        # Create new empty config file
+        with open(os.path.abspath(CONFIG_PATH), 'wb') as config_file:
+            config = {}
+            pickle.dump(config, config_file)
+    return config
+
+
+def save_config(config):
+    """
+    Save config to file
+    """
+    with open(os.path.abspath(CONFIG_PATH), 'wb') as config_file:
+        pickle.dump(config, config_file)
+    return config
