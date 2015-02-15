9ed4890895ce69a431ba60313bec8e9b230b2df5
Yauhen Shulitski <jsnjack@gmail.com>
2015-02-15 14:31:57 +0300

    Style Add account dialog

--- dialogs/add_account.py
+++ dialogs/add_account.py
@@ -3,13 +3,16 @@ from gi.repository import Gtk
 
 class AddAccountDialog(Gtk.Dialog):
     def __init__(self, parent):
-        Gtk.Dialog.__init__(self, "My Dialog", parent, 0, use_header_bar=True,
+        Gtk.Dialog.__init__(self, "Add account", parent, 0, use_header_bar=True,
                             buttons=(Gtk.STOCK_APPLY, Gtk.ResponseType.OK, Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
         self.set_modal(True)
 
         # Set focus to cancel button (so that placeholder text will be visible)
         cancel_button = self.get_header_bar().get_children()[0]
         self.set_focus(cancel_button)
+
+        apply_button = self.get_header_bar().get_children()[1]
+        apply_button.get_style_context().add_class("suggested-action")
 
         username_entry = Gtk.Entry(placeholder_text=u"Username")
         password_entry = Gtk.Entry(placeholder_text=u"Password")
